f522b75bb2f77be3ca6fad3f003e52a34b639930
Sakthi SR <sakthisr@Sakthis-MacBook-Air.local>
2026-01-31 11:48:43 +0530

    Pushing user inpput file code

---
+++ userinput.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter user name"
+
+read -s username
+
+echo "user entered id is:$USERNAME"
+
+echo "Please enter password"
+
+read -s password
+
+echo "Entered password if:$PASSWORD"
